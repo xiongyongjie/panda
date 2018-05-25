3d407dba7a3348e245f053dc72eed1de28a11105
xiongyongjie <xyj126com>
2018-05-26 00:22:58 +0800

    提交价格和商品模块model及servcie

---
+++ service/price/src/main/java/com/springcloud/price/PriceApplication.java
@@ -0,0 +1,16 @@
+package com.springcloud.price;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+public class PriceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PriceApplication.class, args);
+    }
+}
