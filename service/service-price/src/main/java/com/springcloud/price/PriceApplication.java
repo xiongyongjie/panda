0de59d3f0c86c5ab9d67fbb91cc643415b056b1f
xiongyongjie <1144303569@qq.com>
2018-05-26 18:04:51 +0800

    配置文件修改提交

--- service/service-price/src/main/java/com/springcloud/price/PriceApplication.java
+++ service/service-price/src/main/java/com/springcloud/price/PriceApplication.java
@@ -1,16 +1,17 @@
-package com.springcloud.product;
+package com.springcloud.price;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaClient
 @EnableFeignClients
-public class ProductApplication {
+public class PriceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ProductApplication.class, args);
+        SpringApplication.run(PriceApplication.class, args);
     }
 }
--- service/service-product/src/main/java/com/springcloud/product/ProductApplication.java
+++ service/service-product/src/main/java/com/springcloud/product/ProductApplication.java
@@ -1,16 +1,17 @@
-package com.springcloud.price;
+package com.springcloud.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaClient
 @EnableFeignClients
-public class PriceApplication {
+public class ProductApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(PriceApplication.class, args);
+        SpringApplication.run(ProductApplication.class, args);
     }
 }
